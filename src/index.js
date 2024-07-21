7fcf6fe5380503540f01c8f5a0e9ff50c4a0641e
faizitguy <faiz.itguy@gmail.com>
2024-07-21 11:17:28 +0530

    Added Logger with winston package

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { PORT } = require('./config');
+const { ServerConfig, Logger } = require('./config');
 const apiRoutes = require('./routes/index')
 
 const app = express();
 
 app.use('/api', apiRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Started server at port: ${PORT}`);
+app.listen(ServerConfig.PORT, () => {
+    console.log(`Started server at port: ${ServerConfig.PORT}`);
+    // Logger.info("Successfully Started the server", "root", {})
 })
